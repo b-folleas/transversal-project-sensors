9b2b2143995415dd0fcbc2cead807ffce8cf88c9
AmazingBrice <brice.folleas@gmail.com>
2020-12-09 15:13:28 +0100

    Add new acknowledge verification from gateway

--- gateway/gateway-controller.py
+++ gateway/gateway-controller.py
@@ -1,16 +1,28 @@
+# coding: utf8
+
 from microbit import *
 import radio
+
+GATEWAY_PIN = '98'
+BROADCAST_PIN = '99'
+FULL_MESSAGE = ""
 
 radio.on()
 radio.config(channel=1)  # Setting channel for communication
 radio.config(power=7)
-SENSOR_PIN = '98'
-FULL_MESSAGE = ""
 
 def radio_handle(packet):
+
+    global FULL_MESSAGE
+    global BROADCAST_PIN
+    global GATEWAY_PIN
+
+    print('New packet')
     print(packet)
+
     source_pin = packet[0:2]
     print("Source pin : " , source_pin)
+
     destination_pin = packet[2:4]
     print("destination_pin : " , destination_pin)
 
@@ -25,58 +37,41 @@ def radio_handle(packet):
 
     data = packet[11:]
     print("data : " , data)
-    global FULL_MESSAGE
     
-    if flag == 'SYN':
-        response = SENSOR_PIN + source_pin + '00' + '00' + 'ACK' + '##################' # 99 for broadcast
-        radio.send(response) # packet sending process continue
-    elif flag == 'PSH':
-        FULL_MESSAGE = FULL_MESSAGE + data
-        print(data)
-        # continuer communication avec sensor -> check sensor_pin
-    elif flag == 'FIN':
-        FULL_MESSAGE = FULL_MESSAGE + data
-        print(data.strip('d'))
-        print(FULL_MESSAGE.strip('#'))
-        FULL_MESSAGE = ""
-        # arreter communication avec sensor -> pret à accepter un nouveau syn
-    elif flag == 'RST':
-        radio.send('RST')
-        # arreter communication avec sensor -> n'écrit pas en base le message de la derniere communication du sensor qui a envoyé le reset
+    if (destination_pin == GATEWAY_PIN or destination_pin == BROADCAST_PIN):
+        if (flag == 'SYN'):
+            response = GATEWAY_PIN + source_pin + '00' + '00' + 'ACK' + '##################'
+            radio.send(response) # packet sending process continue
+        elif flag == 'PSH':
+            FULL_MESSAGE = FULL_MESSAGE + data
+            print(data)
+            # continuer communication avec sensor -> check sensor_pin
+        elif flag == 'FIN':
+            FULL_MESSAGE = FULL_MESSAGE + data
+            print('Message :', FULL_MESSAGE.strip('#')) # delete padding
+            FULL_MESSAGE = ""
+            # arreter communication avec sensor -> pret à accepter un nouveau syn
+        elif flag == 'RST':
+            radio.send('RST')
+            # arreter communication avec sensor -> n'écrit pas en base le message de la derniere communication du sensor qui a envoyé le reset
+        else:
+            print('Error : Unauthorized Flag.')
     else:
-        print('Error : Unauthorized Flag.')
+        print('Error : Wrong destination')
 
-while True:
-    msg = radio.receive()
+if __name__ == "__main__" :
 
-    if (msg != None):
-        radio_handle(msg)
+    while True:
+        msg = radio.receive()
 
-    if button_a.is_pressed():
-        print("Gateway test")
-     
-    # stop while
-    if button_a.is_pressed() and button_b.is_pressed():
-        print('Execution stopped')
-        break 
+        if (msg != None):
+            radio_handle(msg)
+
+        if button_a.is_pressed():
+            print("Gateway test")
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
+        # stop while
+        if button_a.is_pressed() and button_b.is_pressed():
+            print('Execution stopped')
+            break 
+            
--- sensor/sensor-controller.py
+++ sensor/sensor-controller.py
@@ -7,10 +7,10 @@ from time import *
 # testing msg : 22 caractères max on s'assure toujours que le message est inférieur a 176 bits (22 caractères)
 msg = "(1,1,1)(2,2,2)(3,3,3)(4,4,4)(5,5,5)(6,6,6)(7,7,7)(8,8,8)"
 
-
 PACKET_MAX_LENGTH = 29
 SENSOR_PIN = '01'
-GATEWAY_PIN = ''
+BROADCAST_PIN = '99'
+GATEWAY_PIN = None
 
 uart.init(baudrate=115200, bits=8, parity=None, stop=1)
 
@@ -19,37 +19,61 @@ radio.config(channel=1)
 radio.config(power=7)
 
 def init_connection():
-    packet = SENSOR_PIN + '99' + '00' + '00' + 'SYN' + '######################' # 99 for broadcast
+
+    global SENSOR_PIN
+    global BROADCAST_PIN
+
+    packet = SENSOR_PIN + BROADCAST_PIN + '00' + '00' + 'SYN' + '######################' # 99 for broadcast
     print("init radio : " , packet) # debug
-    try :
+    try:
         radio.send(packet)
     except ValueError:
         print("Error : Connection failure.")
-    sleep(0.1)  # sleep 100 ms
-   
+    sleep(1)  # sleep 1s before trying new connection
    
 def uart_handle():
     msg_bytes = (uart.read())
     msg_str = str(msg_bytes, 'UTF-8') # encoding 
     # so far, do nothing
 
-
 def uart_send(msg):
     print(msg)
 
 def radio_handle(packet): # response from gateway
-    print('Got message', packet) # debug to check if the packet
-    source_pin = packet[0:1]
-    destination_pin = packet[2:3]
-    communication_id = packet[4:5]
-    packet_id = packet[5:6]
-    flag = packet[7:10]
-    data = packet[10:]
 
-    if flag == 'ACK':
+    global GATEWAY_PIN
+
+    print('Got message', packet) # debug to check if the packet
+    source_pin = packet[0:2]
+    destination_pin = packet[2:4]
+    communication_id = packet[4:6]
+    packet_id = packet[6:8]
+    flag = packet[8:11]
+    data = packet[11:]
+
+    if flag == 'ACK': # can communicate with gateway
         GATEWAY_PIN = source_pin
     
 def radio_send(msg): # split the msg into packets of defined length
+    packet_id = 0
+    flag = 'PSH'
+    for i in range(0, len(msg)-1, 18):
+        subMsg = msg[i:i+18]
+        while len(subMsg) < 18 :
+            subMsg+='#' # padding
+            flag = 'FIN'
+        packet = SENSOR_PIN + GATEWAY_PIN + '00' + (str(packet_id) if packet_id > 9 else '0' + str(packet_id)) + flag + subMsg
+        if (len(packet) <= PACKET_MAX_LENGTH):    
+            try:
+                # print(packet) # debug
+                radio.send(packet)
+            except ValueError:
+                print("Error : There is a problem with sending radio messages.")
+        else:
+            print("Error : Packet segmentation.")
+        packet_id += 1
+
+def radio_send_save(msg): # split the msg into packets of defined length
     packet_id = 0
     flag = 'PSH'
     for i in range(0, len(msg)-1, 18):
@@ -68,45 +92,29 @@ def radio_send(msg): # split the msg into packets of defined length
             print("Error : Packet segmentation.")
         packet_id += 1
 
-print('Hello')
+if __name__ == "__main__" :
 
-while True:
+    while (GATEWAY_PIN == None):
+        # keep asking for a connection
+        init_connection()
 
-    init_connection()
+        # then sensor keep waiting for a gateway
+        packet_received = radio.receive()
 
-    # we suggest the sensor only listen if it is awaiting an ACK
-    packet_received = radio.receive()
+        if (packet_received != None):
+            radio_handle(packet_received) # expecting ACK to continue communication
+            if (GATEWAY_PIN != None):
+                print('GATEWAY:',GATEWAY_PIN)
+                radio_send_save(msg)           
+            else :
+                print('Error : Connection refused')
+        
+        sleep(1)
 
-    if (packet_received != None):
-        if (packet_received == 'ACK'):
-            radio_send(msg)
-        else :
-            print('Error : Connection refused')
-       
-    sleep(1)
-
-    if uart.any(): # check if there is anything to be read
-        uart_handle()
-     
-    # stop while
-    if button_a.is_pressed() and button_b.is_pressed():
-        print('Execution stopped')
-        break
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-    
+        if uart.any(): # check if there is anything to be read
+            uart_handle()
+        
+        # stop while
+        if button_a.is_pressed() and button_b.is_pressed():
+            print('Execution stopped')
+            break
