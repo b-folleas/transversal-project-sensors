b4051169100354ddae9575d1a2d812bd76fe0419
AmazingBrice <brice.folleas@gmail.com>
2020-12-11 18:34:28 +0100

    Add address unicast

--- gateway/gateway-controller.py
+++ gateway/gateway-controller.py
@@ -2,19 +2,25 @@
 
 from microbit import *
 import radio
+import random
 
 GATEWAY_PIN = '98'
 BROADCAST_PIN = '99'
 FULL_MESSAGE = ''
-COMMUNICATION_ID = 0
-PACKET_ID = 0
+COMMUNICATION_ID = 0  # From 0 to 100, set the link between gateway and sensor
+PACKET_ID = 0  # Last packet got on gateway
+STOP_BOOL = False
+DEFAULT_ADDRESS = 75626974
 
 radio.on()
 radio.config(channel=1)  # Setting channel for communication
 radio.config(power=7)
+radio.config(address=DEFAULT_ADDRESS)
+
 
 def id_2_char(id):
     return (str(id) if id > 9 else '0' + str(id))
+
 
 def radio_handle(packet):
 
@@ -24,36 +30,34 @@ def radio_handle(packet):
     global COMMUNICATION_ID
     global PACKET_ID
 
-    print('New packet')
+    print('Incoming packet :')
     print(packet)
 
     source_pin = packet[0:2]
-    print('Source pin : ' , source_pin)
-
     destination_pin = packet[2:4]
-    print('destination_pin : ' , destination_pin)
-
     communication_id = packet[4:6]
-    print('communication_id : ' , communication_id)
-
     packet_id = packet[6:8]
-    print('packet_id : ' , packet_id)
-
     flag = packet[8:11]
-    print('flag : ' , flag)
-
     data = packet[11:]
-    print('data : ' , data)
     if (destination_pin == (BROADCAST_PIN or GATEWAY_PIN)):
-        if (flag == 'SYN'): # réponds ACK
-            COMMUNICATION_ID = (COMMUNICATION_ID + 1)%100 # each ACK sent creates indent COMMUNICATION_ID
-            print('Set COMMUNICATION_ID :', COMMUNICATION_ID)
-            response = GATEWAY_PIN + source_pin + id_2_char(COMMUNICATION_ID) + '00' + 'ACK' + '##################'
-            radio.send(response) # ACK sent, packet sending process continue
-        elif (flag == 'RST'):
-            response = GATEWAY_PIN + source_pin + communication_id + packet_id + 'RST' + '##################'
-            radio.send(response)
-            # arreter communication avec sensor -> n'écrit pas en base le message de la derniere communication du sensor qui a envoyé le reset
+        if (flag == 'SYN'):  # response from gateway is ACK
+            # each ACK sent creates COMMUNICATION_ID
+            COMMUNICATION_ID = (COMMUNICATION_ID + 1) % 100
+            # New address for unicast is sent in DATA of ACK
+
+            # Find right pool for random address
+            address = random.randint(75626970, 75626980)
+            print('Set New address :', address)  # debug
+
+            response = GATEWAY_PIN + source_pin + \
+                id_2_char(COMMUNICATION_ID) + '00' + 'ACK' + str(address)
+            radio.send(response)  # ACK sent, packet sending process continue
+
+            # Setting address for communication after sending packet (because sensor is still on old adress)
+            try:
+                radio.config(address=address)
+            except ValueError:
+                print('Error : Wrong address setting')
     elif (destination_pin == GATEWAY_PIN):
         if (flag == 'PSH'):
             # check le communication_id
@@ -61,37 +65,52 @@ def radio_handle(packet):
                 FULL_MESSAGE = FULL_MESSAGE + data
                 print(data)
             else:
-                print('Error : Wrong Communication ID :', communication_id, 'should be :', id_2_char(COMMUNICATION_ID))
+                print('Error : Wrong Communication ID :', communication_id,
+                      'should be :', id_2_char(COMMUNICATION_ID))
         elif (flag == 'FIN'):
             if (id_2_char(COMMUNICATION_ID) == communication_id):
                 if (packet_id == id_2_char(PACKET_ID + 1)):
                     # check communication_id & packet_id = packet_id +1
                     FULL_MESSAGE = FULL_MESSAGE + data
-                    print('Message :', FULL_MESSAGE.strip('#')) # delete padding
+                    # delete padding
+                    print('Message :', FULL_MESSAGE.strip('#'))
                     FULL_MESSAGE = ""
                     PACKET_ID = packet_id
+
+                    # fin de la communication, on repasse sur l'adresse par défaut
+                    print('Back to default address :',
+                          int(DEFAULT_ADDRESS))  # debug
+                    radio.config(address=int(DEFAULT_ADDRESS))
                 else:
-                    print('Error : Wrong Packet ID :', packet_id, 'should be :', PACKET_ID + 1)
+                    print('Error : Wrong Packet ID :', packet_id,
+                          'should be :', PACKET_ID + 1)
+                    # keep the previous COMMUNICATION_ID
+                    response = GATEWAY_PIN + source_pin + \
+                        id_2_char(COMMUNICATION_ID) + '00' + \
+                        'RST' + '##################'
+                    radio.send(response)  # RST sent
+                    radio.config(address=DEFAULT_ADDRESS)
             else:
-                print('Error : Wrong Communication ID :', communication_id, 'should be :', id_2_char(COMMUNICATION_ID))
+                print('Error : Wrong Communication ID :', communication_id,
+                      'should be :', id_2_char(COMMUNICATION_ID))
     else:
         print('Error : Unauthorized Flag.')
 
-if __name__ == '__main__' :
+
+if __name__ == '__main__':
 
     print('I am gateway')
 
-    while True:
+    while not STOP_BOOL:
         msg = radio.receive()
 
         if (msg != None):
             radio_handle(msg)
 
         if button_a.is_pressed():
-            print("Gateway test")
-        
+            print("Gateway is alive !")
+
         # stop while
         if button_a.is_pressed() and button_b.is_pressed():
             print('Execution stopped')
-            break 
-            
+            STOP_BOOL = True
